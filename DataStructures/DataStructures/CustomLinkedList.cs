41d5b58f6db5732eece73d611d298870af0e2cc8
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-01-27 15:31:59 +0530

    [mahafuz] adding uc1

---
+++ DataStructures/DataStructures/CustomLinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class CustomLinkedList
+    {
+       
+            public Node head;
+            //Method to Add Node in Linked List
+            public void AddNode(int data)
+            {
+                Node newnode = new Node(data);
+                //condition to Check head and add first Node.
+                if (this.head == null)
+                {
+                    this.head = newnode;
+                }
+                else
+                {
+                    var lastNode = GetLastNode();
+                    lastNode.next = newnode;
+                }
+                Console.WriteLine("inserted into node: " + newnode.data);
+            }
+
+            public Node GetLastNode()
+            {
+                //Temp Node to reference to iterate through nodes & not lose or break node
+                Node temp = head;
+                //Loop to check next address in Node.
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                return temp;
+            }
+        
+    }
+}
---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataStructures;
+
+CustomLinkedList customLinkedList = new CustomLinkedList();
+customLinkedList.AddNode(56);
+customLinkedList.AddNode(30);
+customLinkedList.AddNode(76);
+Console.ReadLine();
